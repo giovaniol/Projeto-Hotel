1858b8da3e9c5408ebd9f5692644d5178e0c1c3b
Giovani Oliveira <giovani.oliveiracpv@gmail.com>
2023-12-13 15:42:58 -0300

    Documentação do Documento

--- model/Documento.java
+++ model/Documento.java
@@ -1,27 +1,59 @@
 package model;
 
+/**
+ * A classe Documento representa um documento com número e tipo.
+ * Exemplo de uso: new Documento("123456789", TipoDocumento.CPF).
+ *
+ * @author Seu Nome
+ *
+ * @see TipoDocumento
+ */
 public class Documento {
     private String numero;
     private TipoDocumento tipo;
 
-    public Documento(String numero, TipoDocumento tipagem) {
+    /**
+     * Constrói um objeto Documento com o número e tipo especificados.
+     *
+     * @param numero O número do documento.
+     * @param tipo   O tipo de documento.
+     * @throws IllegalArgumentException Se o número do documento for nulo ou vazio.
+     */
+    public Documento(String numero, TipoDocumento tipo) {
         setNumero(numero);
-        setTipo(tipagem);
+        setTipo(tipo);
+
     }
 
+    /**
+     * Obtém o número do documento.
+     *
+     * @return O número do documento.
+     */
     public String getNumero() {
         return numero;
     }
 
-
+    /**
+     * Obtém o tipo de documento.
+     *
+     * @return O tipo de documento.
+     */
     public TipoDocumento getTipo() {
         return tipo;
     }
 
     private void setNumero(String numero) {
+        if (numero == null || numero.trim().isEmpty()) {
+            throw new IllegalArgumentException("Número do documento não pode ser nulo ou vazio.");
+        }
         this.numero = numero;
     }
-    private void setTipo(TipoDocumento tipagem) {
-        this.tipo = tipagem;
+
+    private void setTipo(TipoDocumento tipo) {
+        if (tipo == null) {
+            throw new IllegalArgumentException("Tipo de documento não pode ser nulo.");
+        }
+        this.tipo = tipo;
     }
 }
